d2b54ab865e72e9ccab79ce1a1d98b92972acab2
CT Smyth <cliff.smyth@gmail.com>
2019-01-25 09:53:03 -0400

    Added beanx, other minor

--- Interfaces.sol
+++ Interfaces.sol
@@ -2,26 +2,37 @@ pragma solidity ^0.4.24;
 
 import "./UBI.sol";
 
-contract BeansInterface internal {
+contract BeansInterface {
 transfer(address _to, uint256 _value) returns (bool success);
+}
+
+contract BeanXInterface{
+  function ownerOf(uint256 tokenId) public view returns (address owner);
 }
 
 contract Interfaces is UBI {
 
   BeansInterface beansContract;
-/*
-  modifier onlyOwnerOf(uint _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
-    _;
-  }
-*/
+
   function setBeansContractAddress(address _address) external onlyOwner {
     beansContract = BeansInterface(_address);
   }
 
-
-  function transfer(address _to, uint256 _value) returns (bool) {
+  function transfer(address _to, uint256 _value) internal returns (bool) {
     bool success;
     success = beansContract.transfer(_to,_value);
+    return success
+  }
+
+
+
+    function setBeanXContractAddress(address _address) external onlyOwner {
+    beanXContract = BeanXInterface(_address);
+  }
+
+  function ownerOf(uint256 tokenId) public view returns (address);
+    bool owner;
+    owner = beanXContract.ownerOf(tokenId);
+    return owner
   }
 }
