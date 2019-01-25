60d19248d8b7a49c7789de1d68b09bc9ea9ab5c9
CT Smyth <cliff.smyth@gmail.com>
2019-01-25 10:29:29 -0400

    whitespace

--- UBI.sol
+++ UBI.sol
@@ -65,7 +65,6 @@ contract distribute_beans is Ownable {
         
         // Remember to zero the pending refund before
         // sending to prevent re-entrancy attacks
-        
         pendingWithdrawals[msg.sender] = 0;
         
         transfer(msg.sender, amount);    //TEST!!!!!!!!!!
