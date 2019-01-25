5c92eb19cc1979d9bf49453fb90714ac114772db
CT Smyth <cliff.smyth@gmail.com>
2019-01-25 09:38:47 -0400

    minor

--- Interfaces.sol
+++ Interfaces.sol
@@ -3,19 +3,7 @@ pragma solidity ^0.4.24;
 import "./UBI.sol";
 
 contract BeansInterface {
-/*  function getKitty(uint256 _id) external view returns (
-    bool isGestating,
-    bool isReady,
-    uint256 cooldownIndex,
-    uint256 nextActionAt,
-    uint256 siringWithId,
-    uint256 birthTime,
-    uint256 matronId,
-    uint256 sireId,
-    uint256 generation,
-    uint256 genes
-  );
-  */
+transfer(address _to, uint256 _value) returns (bool success);
 }
 
 contract Interfaces is UBI {
