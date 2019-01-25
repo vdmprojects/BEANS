60f7eb86325a7fecdb1499a41fcb2f5ddb6bf652
CT Smyth <cliff.smyth@gmail.com>
2019-01-25 09:56:42 -0400

    Fixed Beanx interface, comments

--- Interfaces.sol
+++ Interfaces.sol
@@ -12,25 +12,27 @@ contract BeanXInterface{
 
 contract Interfaces is UBI {
 
+//BEANS (ERC20)
   BeansInterface beansContract;
 
   function setBeansContractAddress(address _address) external onlyOwner {
     beansContract = BeansInterface(_address);
   }
 
-  function transfer(address _to, uint256 _value) internal returns (bool) {
+  function transfer(address _to, uint256 _value) internal returns (bool) {  //internal makes this safe???
     bool success;
     success = beansContract.transfer(_to,_value);
     return success
   }
 
-
+//BEANX (ERC721)
+   BeanXInterface beanXContract;
 
     function setBeanXContractAddress(address _address) external onlyOwner {
     beanXContract = BeanXInterface(_address);
   }
 
-  function ownerOf(uint256 tokenId) public view returns (address);
+  function ownerOf(uint256 tokenId) public view returns (address);  //public because why not
     bool owner;
     owner = beanXContract.ownerOf(tokenId);
     return owner
