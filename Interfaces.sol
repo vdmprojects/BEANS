e5ac9d6c6d8888b81250c1abb8038091f1cfe268
CT Smyth <cliff.smyth@gmail.com>
2019-01-25 09:31:56 -0400

    Prelim editing of interfaces.sol
    beans transfer code

--- Interfaces.sol
+++ Interfaces.sol
@@ -1,9 +1,9 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.4.24;
 
 import "./UBI.sol";
 
-contract KittyInterface {
-  function getKitty(uint256 _id) external view returns (
+contract BeansInterface {
+/*  function getKitty(uint256 _id) external view returns (
     bool isGestating,
     bool isReady,
     uint256 cooldownIndex,
@@ -15,44 +15,25 @@ contract KittyInterface {
     uint256 generation,
     uint256 genes
   );
+  */
 }
 
 contract Interfaces is UBI {
 
-  KittyInterface kittyContract;
-
+  BeansInterface beansContract;
+/*
   modifier onlyOwnerOf(uint _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
     _;
   }
-
-  function setKittyContractAddress(address _address) external onlyOwner {
-    kittyContract = KittyInterface(_address);
+*/
+  function setBeansContractAddress(address _address) external onlyOwner {
+    beansContract = BeansInterface(_address);
   }
 
-  function _triggerCooldown(Zombie storage _zombie) internal {
-    _zombie.readyTime = uint32(now + cooldownTime);
-  }
 
-  function _isReady(Zombie storage _zombie) internal view returns (bool) {
-      return (_zombie.readyTime <= now);
-  }
-
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal onlyOwnerOf(_zombieId) {
-    Zombie storage myZombie = zombies[_zombieId];
-    require(_isReady(myZombie));
-    _targetDna = _targetDna % dnaModulus;
-    uint newDna = (myZombie.dna + _targetDna) / 2;
-    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
-      newDna = newDna - newDna % 100 + 99;
-    }
-    _createZombie("NoName", newDna);
-    _triggerCooldown(myZombie);
-  }
-
-  function feedOnKitty(uint _zombieId, uint _kittyId) public {
-    uint kittyDna;
-    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-    feedAndMultiply(_zombieId, kittyDna, "kitty");
+  function transfer(address _to, uint256 _value) returns (bool) {
+    bool success;
+    success = beansContract.transfer(_to,_value);
   }
 }
