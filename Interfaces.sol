fbc178435262b855690eb4c0070409cc414e1435
Unknown <drakesmyth@gmail.com>
2019-01-28 10:26:27 -0500

    Debugging Interfaces.sol

--- Interfaces.sol
+++ Interfaces.sol
@@ -1,24 +1,23 @@
 pragma solidity ^0.5.2;
 
 import "./UBI.sol";
+import "./Ownable.sol";
 
 contract BeansInterface {
-  function transfer(address _to, uint256 _value) returns (bool success);
+  function transfer(address _to, uint256 _value) public returns (bool success);
 }
 
 contract BeanXInterface{
   function ownerOf(uint256 tokenId) public view returns (address owner);
 }
 
-contract Interfaces is UBI {
+contract Interfaces is distribute_beans { // is UBI
 
-  address beansContract =0xc014aba62a72a6564ddf0223a2520914efffb405;
-  address beanXContract = 0;
 
 //BEANS (ERC20)
   BeansInterface beansContract;
 
-  function setBeansAddress (address contractAddress) onlyOwner public {  //order of modifiers?
+  function setBeansAddress (address contractAddress) public {  //order of modifiers? // onlyOwner
     require(contractAddress != 0, "Invalid contract address");
     beansContract = contractAddress;  
   }
@@ -26,24 +25,24 @@ contract Interfaces is UBI {
   function transfer(address _to, uint256 _value) internal returns (bool) {  //internal makes this safe???
     bool success;
     success = beansContract.transfer(_to,_value);
-    return (success)
+    return (success);
   }
 
 //BEANX (ERC721)
    BeanXInterface beanXContract;
 
-  function setBeanxAddress (address contractAddress) onlyOwner public  {
+  function setBeanxAddress (address contractAddress) public  { // onlyOwner
     require(contractAddress != 0, "Invalid contract address");
     beanXContract = contractAddress;  
   }
 
-  function ownerOf(uint256 tokenId) public view returns (address);  //public because why not
-    address owner;
+  function ownerOf(uint256 tokenId) public view returns (address owner){  //public because why not
     owner = beanXContract.ownerOf(tokenId);
-    return (owner)
+    return (owner);
   }
+  
 
   function whichAdresses() public view returns(address,address){
-    return (beanXContract, beansContract)
+    return (beanXContract, beansContract);
   }
 }
